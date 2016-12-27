e350290a0c9d6b342cda3f1d695973f5f57a6e8e
tuling56 <tuling56@gmail.com>
2016-12-27 21:54:19 +0800

    完善和简化cpp调用python部分的代码

--- Invoke/cpp_invoke_python/pymodle_1.py
+++ Invoke/cpp_invoke_python/pymodle_1.py
@@ -22,20 +22,31 @@ class Student:
 	def GetName(self):
 		return "[Return]:",self._name
 
-# 参数调用并有返回值
-def fun_para_return(name):
-	print "hello "+name+"!"
-	return "[Return]:hello "+name+"!"
-
 # 参数调用
 def fun_para(name):
 	print name
 	
-	
+
+# 单参数调用并有返回值
+def fun_para_return(name):
+	print "hello "+name+"!"
+	return "[Return]:hello "+name+"!"
+
+
+# 多参数调用并有返回值
+def fun_paras_return(name,age,city):
+	print "your name is: "+ name
+	print "your age  is: "+ str(age)
+	print "your city is: "+ city
+	return "[Return]:hello:"+name+age+city+"!"
+
+
 if __name__ == "__main__":
-	para="para1"
-	fun_para(para)
-	print fun_para_return(para)
+	para1,para2,para3="para1","para2","para3"
+	fun_para(para1)
+	print fun_para_return(para1)
+	print fun_paras_return(para1,para2,para3)
+
 
 	stu=Student()
 	stu.SetName("xiaowang")
