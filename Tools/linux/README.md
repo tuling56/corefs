99f1893ea6da58a8d91e52e44bd09b20926c8fe9
tuling56 <tuling56@gmail.com>
2017-03-17 22:11:11 +0800

    shell字符串分割工具库

--- Tools/linux/README.md
+++ Tools/linux/README.md
@@ -216,3 +216,35 @@ Synopsis:  sendEmail -f ADDRESS [options]
 **参考**
 
 [linux下安装编译ffmpeg](http://www.toutiao.com/a6348252505277841666/)
+
+#### PostgreSQL
+
+##### 安装
+
+```shell
+yum install postgresql-contrib  postgresql postgresql-server
+# 该命令会安装以下的包
+postgresql        	# PostgreSQL client programs 
+postgresql-contrib  # Extension modules distributed with PostgreSQL
+postgresql-libs     # The shared libraries required for any PostgreSQL clients 
+postgresql-server   # The programs needed to create and run a PostgreSQL server
+uuid             	# 用途未知  
+
+```
+
+初始化
+
+```shell
+# step1:初始化数据库
+/usr/bin/initdb -D /home/yjm/xxx  # 该命令会创建数据库，并完成一系列的配置
+
+# step2:启动数据库
+# Success. You can now start the database server using:
+    postgres -D /home/yjm/xxx
+#or
+	pg_ctl -D /home/yjm/xxx -l logfile start
+```
+
+##### 参考
+
+[Centos上安装和配置PostgreSQL](http://www.linuxidc.com/Linux/2016-09/135538.htm)
---
+++ Tools/shell/utility/str_split.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+cd `dirname $0`
+# ref:http://www.jb51.net/article/56563.htm
+
+# ############################# 形式分割:pgv3_split_c2.xmp_subproduct_other_20170313_21
+function split_s1_m1()
+{
+	local line='pgv3_split_c2.xmp_subproduct_other_20170313_21'
+	echo $line
+	iArray=(${line//./ })
+	db=${iArray[0]}
+	itable=${iArray[1]}
+
+	tArray=(${itable//_/ })
+	tArrayLen=${#tArray[@]}
+	dl=$(($tArrayLen-2))
+	hl=$(($dl-1))
+
+	# 获取表
+	table=
+	for((i=0;i < $dl;i++));do
+		if [ $i -ne $hl ];then
+			table="$table${tArray[$i]}_"
+		else
+			table="$table${tArray[$i]}"
+		fi
+	done
+
+	# 获取日期和实际
+	local date=${tArray[($tArrayLen-2)]}
+	local hour=${tArray[($tArrayLen-1)]}
+
+	echo "[db]:"$db
+	echo "[table]:"$table
+	echo "[date]:"$date
+	echo "[hour]:"$hour
+}
+
+function split_s1_m2()
+{
+	local line='pgv3_split_c2.xmp_subproduct_other_20170313_21'
+	echo $line
+	local db=${line%.*}
+	local table_day_hour=${line#*.}
+	local hour=${table_day_hour##*_}
+
+	local table_day=${table_day_hour%_*}
+	local day=${table_day##*_}
+
+	local table=${table_day%_*}
+
+
+	echo "[db]:"$db
+	echo "[table]:"$table
+	echo "[day]:"$day
+	echo "[hour]:"$hour
+}
+
+
+
+
+# ############################# 形式分割:pgv3_split_c2.xmp_subproduct_other_2017031321
+function split_s2_m1()
+{
+	local line='pgv3_split_c2.xmp_subproduct_other_2017031321'
+	echo $line
+	local iArray=(${line//./ })
+	local db=${iArray[0]}
+	local itable=${iArray[1]}
+
+	local tArray=(${itable//_/ })
+	local tArrayLen=${#tArray[@]}
+	local tbl=$(($tArrayLen-1))
+	local tbl_l1=$(($tbl-1))
+
+	# 获取表
+	local table=
+	for((i=0;i < $tbl;i++));do
+		if [ $i -ne ${tbl_l1} ];then
+			table="$table${tArray[$i]}_"
+		else
+			table="$table${tArray[$i]}"
+		fi
+	done
+
+	# 获取日期和实际
+	local dh=${tArray[($tArrayLen-1)]}
+	local day=${dh:0:8}
+	local hour=${dh:8:2}
+
+	echo "[db]:"$db
+	echo "[table]:"$table
+	echo "[day]:"$day
+	echo "[hour]:"$hour
+}
+
+
+function split_s2_m2()
+{
+	local line='pgv3_split_c2.xmp_subproduct_other_2017031321'
+	echo $line
+	local db=${line%.*}
+	local itable=${line#*.}
+	local table=${itable%_*}
+	local dhour=${line##*_}
+	local day=${dhour:0:8}
+	local hour=${dhour:8:10}
+
+
+	echo "[db]:"$db
+	echo "[table]:"$table
+	echo "[day]:"$day
+	echo "[hour]:"$hour
+
+}
+
+
+
+
+# 测试入口
+split_s1_m2
